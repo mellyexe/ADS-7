b8588afb4c93e72a08f86362315c63e5a189fe7c
mellyexe <125124784+mellyexe@users.noreply.github.com>
2023-04-30 22:20:58 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -2,14 +2,79 @@
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
-template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
+#include <string>
 
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prior;
 };
+
+template<typename T>
+class TPQueue {
+ private:
+    struct Node {
+        T data;
+        Node* next;
+    };
+    Node* head;
+    Node* create(const T&);
+ public:
+    TPQueue();
+    ~TPQueue();
+    void push(const T&);
+    T pop();
+};
+
+template <typename T>
+TPQueue<T>::TPQueue() : head(nullptr) {}
+
+template <typename T>
+TPQueue<T>::~TPQueue() {
+    while (head) {
+        pop();
+    }
+}
+
+template <typename T>
+typename TPQueue<T>::Node* TPQueue<T>::create(const T& data) {
+    Node* temp = new Node;
+    temp->data = data;
+    temp->next = nullptr;
+    return temp;
+}
+
+template <typename T>
+void TPQueue<T>::push(const T& data) {
+    if (head == nullptr) {
+        head = create(data);
+    } else {
+        Node* temp = head;
+        Node* item = create(data);
+        if (data.prior > head->data.prior) {
+            item->next = head;
+            head = item;
+        } else {
+            while (temp->next != nullptr && data.prior <= temp->next->data.prior) {
+                temp = temp->next;
+            }
+            item->next = temp->next;
+            temp->next = item;
+        }
+    }
+}
+
+template <typename T>
+T TPQueue<T>::pop() {
+    if (head == nullptr) {
+        throw std::string("Empty!");
+    } else {
+        Node* temp = head->next;
+        T data = head->data;
+        delete head;
+        head = temp;
+        return data;
+    }
+}
+
 
 #endif  // INCLUDE_TPQUEUE_H_
